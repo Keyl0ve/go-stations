889692189ab9aa574fd24df5fd40347e113dd848
Keyl0ve <kyo.cat34@gmail.com>
2022-11-22 16:23:52 +0900

    add 02

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"os"
 	"time"
 
@@ -51,6 +52,7 @@ func realMain() error {
 	mux := router.NewRouter(todoDB)
 
 	// TODO: サーバーをlistenする
+	log.Fatal(http.ListenAndServe(":8080", mux))
 
 	return nil
 }
